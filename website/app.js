c862ed4d2369752860241e1233f33d580bd3dd34
Amr <72883532+3abghany@users.noreply.github.com>
2021-06-07 14:53:49 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -10,8 +10,8 @@ let d = new Date();
 let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 //URL and Personal API Key for OpenWeatherMap API
-let apiUrl = 'http://api.openweathermap.org/data/2.5/weather?';
-let apiKey = ',&appid=a0e6ac3cb26fd3e0fb294bc34dc98b74';
+const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?';
+const apiKey = ',&appid=a0e6ac3cb26fd3e0fb294bc34dc98b74&units=metric';
 
 
 //generate event listener
@@ -29,10 +29,76 @@ function performAction(e) {
   //chain Promise that makes a POST request
   .then(function(data){
     temperature = data.main.temp;
-    console.log(temperature);
-    
     postData('http://localhost:8000/post', feeling);
   })
+ 
+  //chain Promise that updates the UI dynamically
+  
+  .then(function(){
+  
+    fetchUiData('http://localhost:8000/get')
+
+   })
+ 
+
+ 
+
+}
+
+
+
+//asynchronous function to to make a GET request using fetch()
+async function fetchData(zip){
+
+if(!zip) {
+  alert('no zip code provided');
+}
+
+const response = await fetch(`${apiUrl}zip=${zip}${apiKey}`)
+
+const data = await response.json()
+
+console.log(data);
+
+return data;
+}
+
+//asynchronous function to to make a POST request
+async function postData(url, feelings){
+
+const dataS={
+  date: newDate,
+  temp: temperature,
+  userRes: feelings
+}
+
+const options =  {
+  method: "POST",
+  credentials: "same-origin",
+  headers: {
+    "Content-Type": "application/json"
+            },
+  body: JSON.stringify(dataS)
+          }
+
+await fetch(url, options);
+
+}
+
+//asynchronous function GET request&update ui based on the server response
+async function fetchUiData(url){
+    const serverResponse = await fetch(url);
+    const serverData = await serverResponse.json();
+    console.log(serverData);
+    
+    resDate.innerHTML = `Date is: ${serverData.date}`;
+    resTemp.innerHTML = `Temperature is: ${serverData.temp}`;
+    resContent.innerHTML = `Content is: ${serverData.userRes}`;
+    
+    
+    
+}
+
  
   //chain Promise that updates the UI dynamically
   .then(function(data){
